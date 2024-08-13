40a74e63310b0f4c17b29240b0bd4d9fab7a20f8
Adam Paksy <adam.paksy@gmail.com>
2024-08-13 20:33:07 +0200

    vhdx chaos

---
+++ vhdx_manager.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+#define WIN32_LEAN_AND_MEAN  // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+#include <string>
+
+void CreateVHDX(std::wstring path, ULONGLONG size, ULONG sectorSize);
+int Apple();
